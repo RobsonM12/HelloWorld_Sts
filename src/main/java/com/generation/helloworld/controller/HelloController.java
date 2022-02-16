f25391df24ccfc8152fa4ba095745706f57c9b7a
RobsonM12 <robsonmorais1996@hotmail.com>
2022-02-16 11:17:07 -0300

    Spring aula 1

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -1,5 +1,15 @@
 package com.generation.helloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
+	
+	public String getHello () {
+		return "Hello T44";
+	}
 }
